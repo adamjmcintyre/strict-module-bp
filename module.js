dd7435a7310315bc439dd50623f93833ac7c7408
Adam J. McIntyre <adamjmcintyre@gmail.com>
2012-05-30 21:04:11 -0400

    Added first take on boilerplate

---
+++ module.js
@@ -0,0 +1,14 @@
+;(function(win, $, undef){
+    "use strict";    
+
+    // Check to see if our global is available as a member of window; if it is, our namespace root exists; if not, we'll create it.
+    var foospace = win.foospace = (typeof win.foospace !== undef) ? win.foospace : {};
+
+    (foospace.module = function(){
+        return{
+            _init : function(){
+                // Etc. etc.
+            }
+        }
+    }())._init();
+}(window, jQuery));
